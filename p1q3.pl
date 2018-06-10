223987e20a6065e043e36186ed59266672befe29
Matthew McLaughlin <matthedm@uci.edu>
2018-06-10 16:21:45 -0700

    started merge sort

---
+++ p1q3.pl
@@ -0,0 +1,23 @@
+/*
+mergesort(US,S)
+
+?- mergesort([3,4,5,1,2,6],[1,2,3,4,5,6]).
+
+true
+
+?- mergesort([3,4,5,1,2,6],[1,2,3,4,5]).
+
+false
+
+?- mergesort([3,4,5,1,2,6],T).
+
+T = [1, 2, 3, 4, 5, 6]
+
+mergesort(LIST,OUTLIST):-
+*/
+
+
+
+	
+
+
